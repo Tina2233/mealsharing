90bb21cfeccb5cd5fa4e5196eb2cece2b62ef96d
Ting Wu <18817870108@163.com>
2019-01-09 09:27:08 +0000

    Styling

--- app.js
+++ app.js
@@ -4,22 +4,27 @@ var bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+var campgrounds = [
+      {name: "CA", image:""},
+      {name: "NY", image:""},
+      {name: "Boston", image:""}
+  ]
 
 app.get("/", function(req,res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req,res){
-  var campgrounds = [
-      {name: "CA", image:""},
-      {name: "NY", image:""},
-      {name: "Boston", image:""}
-  ]
   res.render("campgrounds", {campgrounds:campgrounds});
 });
 
-app.post("/campground", function(req,res){
-   res.send("You hit the post route");
+app.post("/campgrounds", function(req,res){
+   //res.send("You hit the post route");
+   var name = req.body.name;
+   var image = req.body.image;
+   var newCampground = {name: name, image: image};
+   campgrounds.push(newCampground);
+   res.redirect("/campgrounds");
 });
 
 app.get("/campgrounds/new", function(req,res){
