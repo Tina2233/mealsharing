d4e57774f174a5ac1d794983e38100d1c7aec9d0
Ting Wu <18817870108@163.com>
2019-01-11 00:48:29 +0000

    Create campground model

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-var campgrounds = [
-      {name: "CA", image:""},
-      {name: "NY", image:""},
-      {name: "Boston", image:""}
-  ];
+
+//Schema Setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image:String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({name: "CA", image:""},function(err, campground){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("NEWLY CREATED CAMPGROUND:");
+//         console.log(campground);
+//     }
+// });
+
 
 app.get("/", function(req,res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req,res){
-  res.render("campgrounds", {campgrounds:campgrounds});
+
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("campgrounds", {campgrounds:allCampgrounds});
+        }
+    });
 });
 
 app.post("/campgrounds", function(req,res){
@@ -23,8 +45,16 @@ app.post("/campgrounds", function(req,res){
   var name = req.body.name;
   var image = req.body.image;
   var newCampground = {name: name, image: image};
-  campgrounds.push(newCampground);
-  res.redirect("/campgrounds");
+  
+  //Create a new campgound and save to DB
+  Campground.create(newCampground, function(err,newlyCreated){
+      if(err){
+          console.log(err);
+      }else{
+          //redirected back to campgrounds page
+          res.redirect("/campgrounds");
+      }
+  });
 });
 
 app.get("/campgrounds/new", function(req,res){
