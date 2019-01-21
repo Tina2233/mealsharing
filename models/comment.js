e8f21a8156c72a01782e04341db98fc0dd08e986
Ting Wu <18817870108@163.com>
2019-01-21 10:49:17 +0000

    Adding user authentication

--- models/comment.js
+++ models/comment.js
@@ -5,14 +5,4 @@ var commentSchema = new mongoose.Schema({
     author: String
 });
 
-// var Comment= mongoose.model("Comment", commentSchema);
-// Comment.create(
-//     {
-//     text: "sdfg",
-//     author:"dfs"
-// }, function(err, comment){
-//     if(err) {console.log(err);}
-//     else{console.log(comment);}
-// });
-
-module.eports = mongoose.model("Comment", commentSchema);
+module.exports = mongoose.model("Comment", commentSchema);
