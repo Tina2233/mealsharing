2c6342be67eca29183151d4c21734a991099c186
Ting Wu <18817870108@163.com>
2019-01-21 18:05:30 +0000

    adding function: flashing message

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var passport    = require("passport");
 var LocalStrategy = require("passport-local");
 var Campground  = require("./models/campground");
 var Comment     = require("./models/comment");
+var flash       = require("connect-flash");
+var methodOverride = require("method-override");
 var User        = require("./models/user");
 var seedDB = require("./seeds");
 
@@ -18,7 +20,9 @@ mongoose.connect("mongodb://localhost/yelp_camp_v6");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-seedDB();
+app.use(methodOverride("_method"));
+app.use(flash());
+//seedDB(); //seed the data base
 
 //passport configuration
 app.use(require("express-session")({
@@ -34,6 +38,8 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
    res.locals.currentUser = req.user;
+   res.locals.error = req.flash("error");
+   res.locals.success = req.flash("success");
    next();
 });
 
